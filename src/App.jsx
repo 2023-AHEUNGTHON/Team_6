9b53dc24d671ae935f29fd20f84bd500cb5fb409
yueunfive <122276414+yueunfive@users.noreply.github.com>
2023-11-25 10:29:28 +0900

    feat/my-page,footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import MyPage from "./pages/MyPage";
+import MyPageEdit from "./pages/MyPageEdit";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/my-page" element={<MyPage />} />
+          <Route path="/my-page-edit" element={<MyPageEdit />} />
         </Routes>
       </Router>
     </div>
