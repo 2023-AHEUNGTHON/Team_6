3fa4d9fd3bc362a511a3ae052e501d9622fe9053
yueunfive <122276414+yueunfive@users.noreply.github.com>
2023-11-25 16:00:59 +0900

    feat/로그인,회원가입,마이페이지(수정) API 연동

--- src/pages/Mainpage/Mainpage.js
+++ src/pages/Mainpage/Mainpage.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // import React from "react";
 // import "./Mainpage.css";
 // import logo1 from "../../img/icon.png";
@@ -45,6 +46,49 @@
 //       </div>
 //     </div>
 //   );
+=======
+// import React from 'react';
+// import './Mainpage.css';
+// import logo1 from '../../img/icon.png';
+// import logo2 from '../../img/univ-us.png';
+// import Polygon from '../../img/Polygon.png';
+
+// function Mainpage(){
+//     return(
+//         <div className="Mainpage">
+//             <div class="logo_wrap">
+//                 <img class="logo1" src={logo1}/>
+//                 <img class="logo2" src={logo2}/>
+//             </div>
+//             <div class="search_wrap">
+//                 <div class="s_text">나와 맞는 팀원을 한번에 찾고 싶다면</div>
+//                 <div ><img class="s_img" src={Polygon}/></div>
+//             </div>
+//             <div class="menu_big_wrap">
+//                 <div class="menu_wrap">
+//                     <div class="m_text">Univ-Us<br/>Category</div>
+//                 </div>
+//             </div>
+//             <div class="container">
+//                 <div class="box">Product Manager</div>
+//                 <div class="box">Design</div>
+//                 <div class="box">Photo</div>
+//                 <div class="box">Web</div>
+//                 <div class="box">Composing</div>
+//                 <div class="box">Video</div>
+//                 <div class="box">IOS</div>
+//                 <div class="box">Lyric</div>
+//                 <div class="box">Vocal</div>
+//                 <div class="box">Android</div>
+//                 <div class="box">Marketing</div>
+//                 <div class="box">Dance</div>
+//                 <div class="box">Server</div>
+//                 <div class="box">Advertisement</div>
+//                 <div class="box">etc</div>
+//             </div>
+//         </div>
+//     )
+>>>>>>> Stashed changes
 // }
 
 // export default Mainpage;
