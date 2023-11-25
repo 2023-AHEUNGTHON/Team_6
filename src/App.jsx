7ef69d0387cdf9beaa6ff758b258723e7a26977c
ssorni <ssorni727@gmail.com>
2023-11-25 15:23:55 +0900

    feat: 필터링 결과 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import DetailPost from "./pages/DetailPost";
 import Chat from "./pages/Chat";
 import ChatList from "./pages/ChatList";
 import Mainpage from "./pages/Mainpage/Mainpage";
+import FilterPage from "./pages/FilterPage/FilterPage"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Router>
         <ScrollToTop />
         <Routes>
+          <Route path="/Filterpage" element={<FilterPage />} />
           <Route path="/Mainpage" element={<Mainpage />} />
           <Route path="/" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
